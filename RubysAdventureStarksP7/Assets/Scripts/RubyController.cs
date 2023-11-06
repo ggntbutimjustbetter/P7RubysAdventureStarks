0940a0ba48e558784135575e69bc9c70ff4e4f88
Tristan Starks <757637@student.dusd.net>
2023-11-06 15:29:09 -0800

    madaD
    Changed ruby script made boxes and door

--- RubysAdventureStarksP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureStarksP7/Assets/Scripts/RubyController.cs
@@ -1,25 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate =10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+
+        currentHealth = maxHealth;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Debug.Log(horizontal);
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime; 
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
     }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" +  maxHealth);
+    }
+    
 }
