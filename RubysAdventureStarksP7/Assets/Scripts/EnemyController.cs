f6590cabb7bf963bcc3b1fd5849cf2dc37c0f173
Tristan Starks <757637@student.dusd.net>
2023-11-17 15:26:43 -0800

    Made ruby and robot move
    Made robot move and ruby also made scripts cleaner.

--- RubysAdventureStarksP7/Assets/Scripts/EnemyController.cs
+++ RubysAdventureStarksP7/Assets/Scripts/EnemyController.cs
@@ -4,30 +4,27 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3.0f;
     public bool vertical;
     public float changeTime = 3.0f;
-    Animator animator;
 
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2d;
+
     float timer;
     int direction = 1;
 
+    Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
-       rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
         animator = GetComponent<Animator>();
     }
 
-    void Update ()
+    void Update()
     {
-        animator.SetFloat("Move X", 0);
-        animator.SetFloat("Move Y", direction);
         timer -= Time.deltaTime;
-
         if (timer < 0)
         {
             direction = -direction;
@@ -37,17 +34,20 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 position = rigidbody2D.position;
-
-        if (vertical)
+        Vector2 position = rigidbody2d.position;
+        if(vertical)
         {
-            position.y = position.y + Time.deltaTime * speed;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+            position.x = position.x + Time.deltaTime * speed * direction;
         }
 
-        rigidbody2D.MovePosition(position);
+      rigidbody2d.MovePosition(position);
     }
 }
--- RubysAdventureStarksP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureStarksP7/Assets/Scripts/RubyController.cs
@@ -1,13 +1,15 @@
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
-    
-    int currentHealth;
+    public float timeeInvincible = 2;
     public int health { get { return currentHealth; } }
+    int currentHealth;
 
     bool isInvincible;
     float invincibleTimer;
@@ -15,26 +17,42 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-    // Start is called before the first frame update
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
-
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-        
-        if(isInvincible)
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
-            if(invincibleTimer < 0 ) 
+            if (invincibleTimer > 0)
+            {
                 isInvincible = false;
+            }
         }
     }
 
@@ -49,27 +67,17 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
-        if(amount < 0)
+        if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
+            {
                 return;
-
+            }
             isInvincible = true;
-            invincibleTimer = timeInvincible;   
+            invincibleTimer = timeeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" +  maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
-
-
-    void Launch()
-    {
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 300);
-
-        animator.Settriggeer("Launch");
-    }
-    
 }
